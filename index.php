e6c4c12db2f8290277d454f13c9044bd6f6753ee
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-04-18 15:52:49 +0200

    rivisto codice e bonus 1 e 2 completati

--- index.php
+++ index.php
@@ -39,6 +39,53 @@
 
     ];
 
+    $fiteredHotels = $hotels;
+ 
+if(!empty($_GET['parking']) ){
+
+  $secArray = [];
+
+  foreach($fiteredHotels as $hotel){
+    if($hotel['parking']){
+      $secArray[] = $hotel;
+    }
+  }
+  $fiteredHotels = $secArray;
+
+}
+
+if(isset($_GET['parking']) && empty($_GET['parking'])){
+
+  $secArray = [];
+
+  foreach($fiteredHotels as $hotel){
+    if(!$hotel['parking']){
+      $secArray[] = $hotel;
+    }
+  }
+  $fiteredHotels = $secArray;
+
+}
+
+
+
+if(!empty($_GET['voto']) ){
+
+  $secArray = [];
+
+  foreach($fiteredHotels as $hotel){
+   if ($hotel['vote'] >= $_GET['voto']) {
+    $secArray[] = $hotel;
+    }
+  }
+
+  $fiteredHotels = $secArray;
+
+}
+
+
+
+
 
     ?>
 
@@ -54,46 +101,60 @@
 </head>
 <body>
 
-    <table  class="table table-striped table-hover">
-        <thead>
-            <tr>
-                <?php 
-                    foreach($hotels[0] as $key => $value ){
-                        echo "<th scope='col'>". $key ."</th>";
-                    }
-                ?>
-            </tr>
-        </thead>
+    <h1 class="text-center mb-5">Hotels</h1>
+    
+<div class="container">
 
-        <tbody>
-            <?php 
-                foreach($hotels as $hotel){
-
-                    echo '<tr>';
-                    foreach($hotel as $information => $value){
-
-                        if($information == 'parking' ){
-
-                            if($value==true){
-                                $value='Si';
-                            }else{
-                                $value='No';
-                            }
-                        }
-                        echo "<td>". $value ."</td>";
-                    }
-                    echo '</tr>';
-                }
-            ?>
-        </tbody>
-
-    </table>
-
+<form action="index.php" method="GET">
+    
+    <input class="me-1" type="radio" name="parking" id="yes" value="1">
+    <label for="yes">Con Parcheggio</label>
 
     
+    <input class="ms-5" type="radio" name="parking" id="no" value=""  >
+    <label for="no">Senza Parcheggio</label>
 
+
+    <label class="ms-5" for="">Voto</label>
+    <input class="me-5" type="number" name="voto" min="1" max="5">
+
+    <button class="btn btn-primary ms-5" type="submit">Send</button>
+    <button class="btn btn-secondary ms-5" type="submit">Reset</button>
+
+</form>
+
+</div>
     
-    
+<div class="container mt-5  table-cnt">
+<table  class="table table-striped table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Nome</th>
+      <th scope="col">Descrizione</th>
+      <th scope="col">Parcheggio</th>
+      <th scope="col">Voto</th>
+      <th scope="col">Distanza dal Centro</th>
+    </tr>
+   
+  </thead>
+  <tbody  >
+ <?php foreach($fiteredHotels as $hotel){
+
+  echo '<tr>';
+  echo '<td>' . $hotel['name'] . '</td>';
+  echo '<td>' . $hotel['description'] . '</td>';
+  echo '<td>' . ($hotel['parking'] ? 'Si': 'No'). '</td>';
+  echo '<td>' . $hotel['vote'] . '</td>';
+  echo '<td>' . $hotel['distance_to_center'] . '</td>';
+  echo '</tr>';
+
+    }
+ ?>
+  </tbody>
+
+</table>
+</div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
